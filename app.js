eb4d3eb6395ad5b967defd0a78430aa05cc172de
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-06-29 00:17:55 +0300

    Update stylesheet

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ function openNav() {
 function closeNav() {
   document.getElementById('myNav').style.width = '0%';
 }
+const divValue = document.querySelector('nav-btn > div');
+const portValue = document.getElementsByClassName('port');
+const abtValue = document.getElementsByClassName('abt');
+const contValue = document.getElementsByClassName('cont');
+
+divValue.addEventListener('click', openNav());
+portValue.addEventListener('click', closeNav());
+abtValue.addEventListener('click', closeNav());
+contValue.addEventListener('click', closeNav());
