92ec25e573b05d85578bccc6d60eb8929935d533
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-01 19:47:28 +0300

    Corect linters

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ const cardDesc = document.querySelectorAll('.section-info');
 const cardTech = document.querySelectorAll('.program-btns');
 const cardBtn = document.querySelectorAll('.myBtn');
 const cards = document.querySelectorAll('.card');
-const card1;
-const card2;
-const card3;
-const card4;
+const card1 = {};
+const card2 = {};
+const card3= {};
+const card4 = {};
 const cardObjects = [
   (card1 = {
     title: cardTitle[0].innerHTML,
