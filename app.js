e803b1681dcb2e7157aae92591e7ba58bf8e793a
elmar8287 <L-mar@inbox.ru>
2021-07-01 19:29:22 +0400

    Add close button

--- app.js
+++ app.js
@@ -193,3 +193,15 @@ for(let i = 0; i < cardObjects.length; i++) {
 
 // Closing
 
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+ if (event.target == modal) {
+ modal.style.display = "none";
+}
+}
+
+
