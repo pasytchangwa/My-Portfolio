213cb5c6ad711d6391623dc8f142a3ac0103556f
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-02 10:47:45 +0300

    Add form attributes and java script code

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-function openNav() {
-  document.getElementById('myNav').style.width = '100%';
-}
 
-function closeNav() {
-  document.getElementById('myNav').style.width = '0%';
-}
 const divValue = document.querySelector('nav-btn > div');
 const portValue = document.getElementsByClassName('port');
 const abtValue = document.getElementsByClassName('abt');
@@ -14,3 +8,25 @@ divValue.addEventListener('click', openNav());
 portValue.addEventListener('click', closeNav());
 abtValue.addEventListener('click', closeNav());
 contValue.addEventListener('click', closeNav());
+
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+
+// Form Validation
+
+const submit = document.getElementById('footer-form');
+const emailInput = document.getElementById('email-detail');
+const invalidMessage = document.getElementById('invalid');
+
+submit.addEventListener('submit', (e) => {
+  if (emailInput.value !== emailInput.value.toLowerCase() && emailInput.value == emailInput.toUpperCase()) {
+    e.preventDefault();
+    alert("Please fill in the 'Your Name' box.");
+    invalidMessage.textContent = 'Please, use only lowercase letter for your email!';
+  }
+});
