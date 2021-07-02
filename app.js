231169da859cf44ef8e0a4237a78d4f28cf2aa07
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-02 11:47:04 +0300

    Remove comment in app.js file

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-
 const divValue = document.querySelector('nav-btn > div');
 const portValue = document.getElementsByClassName('port');
 const abtValue = document.getElementsByClassName('abt');
 const contValue = document.getElementsByClassName('cont');
-
-// // divValue.addEventListener('click', openNav());
-// portValue.addEventListener('click', closeNav());
-// abtValue.addEventListener('click', closeNav());
-// contValue.addEventListener('click', closeNav());
 
 function openNav() {
   document.getElementById('myNav').style.width = '100%';
