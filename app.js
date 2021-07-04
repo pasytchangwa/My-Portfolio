eba0e99c7abf47ac658898f867d896fc86021bfb
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-04 12:50:56 +0300

    Add mobile menu to this branch because my partner accidentally removed it in past merge

--- app.js
+++ app.js
@@ -354,3 +354,23 @@ window.addEventListener('load', () => {
     saveFormDataToLocalStorage(nameInput, emailInput, messageInput);
   }));
 });
+
+// Mobile menu project
+
+const divValue = document.querySelector('nav-btn > div');
+const portValue = document.getElementsByClassName('port');
+const abtValue = document.getElementsByClassName('abt');
+const contValue = document.getElementsByClassName('cont');
+
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+
+divValue.addEventListener('click', openNav());
+portValue.addEventListener('click', closeNav());
+abtValue.addEventListener('click', closeNav());
+contValue.addEventListener('click', closeNav());
